9672a96eeb7f8547197b74614808f4c7c6a12d65
kth919 <kth9193@gmail.com>
2019-12-17 21:56:22 +0900

    graphql로 restapi 불러오기

--- graphql/db.js
+++ graphql/db.js
@@ -1,32 +1,7 @@
-let movies = [
-  { id: 0, name: "star wars", score: 1 },
-  { id: 1, name: "avangers", score: 4 },
-  { id: 2, name: "logan", score: 3 }
-];
+import fetch from "node-fetch";
+const API_URL = "https://yts.lt/api/v2/list_movies.json";
 
-export const getMovies = () => movies;
-
-export const getMovieById = id => {
-  const filteredMovie = movies.filter(movie => movie.id === id);
-  return filteredMovie[0];
-};
-
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: `${movies.length + 1}`,
-    name,
-    score
-  };
-  movies.push(newMovie);
-  return newMovie;
-};
-
-export const deleteMovie = id => {
-  const cleanedMovies = movies.filter(movie => movie.id !== id);
-  if (movies.length > cleanedMovies.length) {
-    movies = cleanedMovies;
-    return true;
-  } else {
-    return false;
-  }
-};
+export const getMovies = (limit, rating) =>
+  fetch(`${API_URL}`)
+    .then(res => res.json())
+    .then(json => json.data.movies);
